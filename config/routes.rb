ad2933a7aa6f6aef8e823fad91f72db7b5a9d894
Alexandra Stroe <alexandra.maria.stroe@gmail.com>
2022-04-14 19:28:34 +0200

    Rails yelp mvp

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :restaurants, only: [ :index, :show, :new, :create ] do
+    resources :reviews, only: [ :new, :create ]
+  end
 end
+  # A visitor can see the list of all restaurants.
+  # get "restaurants", to: "restaurants#index"
+  # A visitor can add a new restaurant, and be redirected to the show view of that new restaurant
+  # get "restaurants/new", to: "restaurants#new"
+  # post "restaurants", to: "restaurants#create"
+  # A visitor can see the details of a restaurant, with all the reviews related to the restaurant.
+  # get "restaurants:/id", to: "restaurants#show", as: "restaurant"
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts "Cleaning database..."
+Restaurant.destroy_all
+
+puts "Creating restaurants..."
+dishoom = { name: "Dishoom", address: "7 Boundary St, London E2 7JE", category: "belgian" }
+pizza_east = { name: "Pizza East", address: "56A Shoreditch High St, London E1 6PQ", category: "italian" }
+sensy = { name: "sensy", address: "Bucharest", category: "french" }
+kunnai = { name: "Kunnai", address: "Bucharest", category: "japanese" }
+max = { name: "Max", address: "Stockholm", category: "chinese" }
+
+[ dishoom, pizza_east, sensy, kunnai, max ].each do |attributes|
+  restaurant = Restaurant.create!(attributes)
+  puts "Created #{restaurant.name}"
+end
+puts "Finished!"
